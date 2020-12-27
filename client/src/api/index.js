de58b462cb61ea77582de68d6fcb6f712dcfe9ae
Dimitar Mitrevski <71487915+DimitarMitrevski@users.noreply.github.com>
2020-12-27 13:59:55 +0100

    Inital commit

---
+++ client/src/api/index.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+const url = 'https://fikt.herokuapp.com/posts';
+
+export const fetchPosts = async() => await axios.get(url);
+export const createPost = async(newPost) => await axios.post(url, newPost);
+export const likePost = async(id) => await axios.patch(`${url}/${id}/likePost`);
+export const updatePost =async (id, updatedPost) =>await axios.patch(`${url}/${id}`, updatedPost);
+export const deletePost = async(id) =>await axios.delete(`${url}/${id}`);
